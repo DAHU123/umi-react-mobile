b06b6ef435402a1b83208f08f0185b135d0fd6f5
Chenlili <986220912@qq.com>
2020-12-11 18:24:48 +0800

    将样式转化成vw

--- .umirc.ts
+++ .umirc.ts
@@ -10,24 +10,13 @@ export default defineConfig({
   extraBabelPlugins: [
     ["import", { "libraryName": "antd-mobile", "libraryDirectory": "es", "style": "css" }]
   ],
-  // extraPostCSSPlugins: [
-  //   postcssPx2vw({
-  //     viewportWidth: 750,
-  //     unitPrecision: 5,
-  //     viewportUnit: 'vw',
-  //     minPixelValue: 1,
-  //   }),
-  // ]
-
-  // extraPostCSSPlugins: [
-  //   require('postcss-flexbugs-fixes'),
-  //   require('postcss-px-to-viewport')({
-  //     viewportWidth: 375, // 视窗的宽度，对应的是我们设计稿的宽度，一般是375
-  //     unitPrecision: 3, // 指定`px`转换为视窗单位值的小数位数（很多时候无法整除）
-  //     viewportUnit: 'vw', // 指定需要转换成的视窗单位，建议使用vw
-  //     selectorBlackList: [], // 指定不转换为视窗单位的类，可以自定义，可以无限添加,建议定义一至两个通用的类名
-  //     minPixelValue: 1, // 小于或等于`1px`不转换为视窗单位，你也可以设置为你想要的值
-  //     mediaQuery: false, // 允许在媒体查询中转换`px`
-  //   }),
-  // ],
+  extraPostCSSPlugins: [
+    require('postcss-px-to-viewport')({
+      viewportWidth: 375,//如果设置成750，antd-mobile的样式会整体缩小1倍，平时在拿到750的设计稿的时候，除以2换算一下就行了
+      unitPrecision: 5,
+      viewportUnit: 'vw',
+      minPixelValue: 1,
+      include: /\/src\//
+    }),
+  ],
 });
