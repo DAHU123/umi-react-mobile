505dd43509335adec10b19c12675fa16e98b9478
Chenlili <986220912@qq.com>
2020-12-11 17:14:54 +0800

    完成一系列配置

--- .umirc.ts
+++ .umirc.ts
@@ -8,6 +8,17 @@ export default defineConfig({
   routes: pageRoute,
   dva: {},
   extraBabelPlugins: [
-    ["import", { "libraryName": "antd-mobile", "style": "css" }]
-  ]
+    ["import", { "libraryName": "antd-mobile", "libraryDirectory": "es", "style": "css" }]
+  ],
+  // extraPostCSSPlugins: [
+  //   require('postcss-flexbugs-fixes'),
+  //   require('postcss-px-to-viewport')({
+  //     viewportWidth: 375, // 视窗的宽度，对应的是我们设计稿的宽度，一般是375
+  //     unitPrecision: 3, // 指定`px`转换为视窗单位值的小数位数（很多时候无法整除）
+  //     viewportUnit: 'vw', // 指定需要转换成的视窗单位，建议使用vw
+  //     selectorBlackList: [], // 指定不转换为视窗单位的类，可以自定义，可以无限添加,建议定义一至两个通用的类名
+  //     minPixelValue: 1, // 小于或等于`1px`不转换为视窗单位，你也可以设置为你想要的值
+  //     mediaQuery: false, // 允许在媒体查询中转换`px`
+  //   }),
+  // ],
 });
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,32 @@
 import React from 'react';
-import styles from './index.less';
-import { Button, WhiteSpace, WingBlank } from 'antd-mobile';
+import { Button, Radio, Accordion, List } from 'antd-mobile';
+
+const RadioItem = Radio.RadioItem;
 
 export default (props: any) => {
+  const onChange = (key: any) => {
+    console.log(key);
+  }
   return (
     <div>
       <Button type="primary">primary</Button>
-      <h1 className={styles.title}>Page index</h1>
+      {/*<h1 className={styles.title}>Page index</h1>*/}
       {props.children}
+      <br/>
+      <Radio className="my-radio" onChange={e => console.log('checkbox', e)}>Agree</Radio>
+      <Accordion accordion openAnimation={{}} className="my-accordion" onChange={onChange}>
+        <Accordion.Panel header="Title 1">
+          <List className="my-list">
+            <List.Item>content 1</List.Item>
+            <List.Item>content 2</List.Item>
+            <List.Item>content 3</List.Item>
+          </List>
+        </Accordion.Panel>
+        <Accordion.Panel header="Title 2" className="pad">this is panel content2 or other</Accordion.Panel>
+        <Accordion.Panel header="Title 3" className="pad">
+          text text text text text text text text text text text text text text text
+        </Accordion.Panel>
+      </Accordion>
     </div>
   );
 }
